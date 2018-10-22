dbdbe0be67ce4c73e5b73ec252cb220e798e20b0
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-10-22 18:09:19 -0500

    completed js code to display json array in DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -4,46 +4,28 @@
 
 // fetch to query data
 fetch("http://localhost:3000/entries") // Fetch from the API
-    .then((entry) => entry.json() )  // Parse as JSON
-    .then(entries => {
-        // What should happen when we finally have the array?
-      
-    
+  .then((journalEntry) => journalEntry.json()) // Parse as JSON
+  .then((parsedEntries) => {
+    // What should happen when we finally have the array?
+
+    parsedEntries.forEach((entry) => {
 
 
-// Push journal entries to the DOM--------------------------------------
+      // Push journal entries to the DOM--------------------------------------
 
-/*
-  Purpose: To create, and return, a string template that
-  represents a single journal entry object as HTML
-
-  Arguments: journalEntry (object)
-*/
-
-
-const makeJournalEntryComponent = function(journalEntry) {
-  // Create your own HTML structure for a journal entry
-  return `
+      const makeJournalEntryComponent = function (journalEntry) {
+        // Create your own HTML structure for a journal entry
+        return `
     <div class = "journalEntry">
-     <h1>${journalEntry.concept}</h1>
-     <h2>${journalEntry.date}</h2>
-     <p>${journalEntry.entry}</p>
-     <p>${journalEntry.mood}</p>
+     <h1>${entry.concept}</h1>
+     <h2>${entry.date}</h2>
+     <p>${entry.entry}</p>
+     <p>${entry.mood}</p>
     </div>
   `
-}
+      }
+      document.querySelector(".entryLog").innerHTML += makeJournalEntryComponent(parsedEntries);
+    })
 
-/*
-    Purpose: To render all journal entries to the DOM
-    Arguments: entries (array of objects)
-*/
-const renderJournalEntries = (entries) => {
-  for(let i = 0; i < journalEntries.length; i++) {
-    document.querySelector(".entryLog").innerHTML += makeJournalEntryComponent(entries[i]);
-  }
-}
 
-// Invoke the render function
-renderJournalEntries(journalEntries)
-
-})
+  })
