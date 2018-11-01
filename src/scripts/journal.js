dd4bd614cf1e0fc2bb87d593aa51eba94207e712
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-10-31 21:32:32 -0500

    added fetch post code to journal.js

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -16,5 +16,32 @@
 */
 API.getJournalEntries().then(entries => renderJournalEntries(entries))
 
+// In your main JavaScript module (journal.js) add a click event listener to the Record Journal Entry button at the bottom of your form. When the user clicks the button, you need to create a new entry in your API. The HTTP method that you use to create resources is POST. Guidance on syntax is provided below.
 
 
+let record = document.getElementById("record_button")
+
+record.addEventListener("click", saveJournalEntry)
+
+function saveJournalEntry() {
+let date = document.getElementById("journalDate").value
+let concept = document.getElementById("conceptsCovered").value
+let entry = document.getElementById("journalEntry").value
+let mood = document.getElementById("dailyMood").value
+
+let savedEntry = {
+    date: date,
+    concept: concept,
+    entry: entry,
+    mood: mood
+}
+
+fetch("http://localhost:8088/entries", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(savedEntry)
+  })
+
+}
